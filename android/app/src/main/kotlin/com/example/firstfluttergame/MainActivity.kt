11cdf05be69abc826d6bb17c706e56d523c873ed
emilieiios <emilie1105@web.de>
2023-06-10 15:01:02 +0200

    initial commit

---
+++ android/app/src/main/kotlin/com/example/firstfluttergame/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.firstfluttergame
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flame/flame.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    GameWidget(
+      game: MyGame(),
+    ),
+  );
+}
+
+class MyGame extends Game {
+  @override
+  void render(Canvas canvas) {
+    // TODO: implement render
+  }
+
+  @override
+  void update(double dt) {
+    // TODO: implement update
+  }
+
+}

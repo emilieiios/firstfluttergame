97acdf02e899b940ea62bc115851fed51ce26d45
emilieiios <emilie1105@web.de>
2023-06-10 18:34:26 +0200

    preparation and components

---
+++ lib/components/card.dart
@@ -0,0 +1,5 @@
+import 'package:flame/components.dart';
+
+class Card extends PositionComponent{
+  
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,8 @@
-import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'klondike_game.dart';
 
 void main() {
-  runApp(
-    GameWidget(
-      game: MyGame(),
-    ),
-  );
-}
-
-class MyGame extends Game {
-  @override
-  void render(Canvas canvas) {
-    // TODO: implement render
-  }
-
-  @override
-  void update(double dt) {
-    // TODO: implement update
-  }
-
+  final game = KlondikeGame();
+  runApp(GameWidget(game: game));
 }
